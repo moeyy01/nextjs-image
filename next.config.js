6af2a63a413cefb2e4845249f62c308b8b1dc02f
moeyys <58381898+moeyys@users.noreply.github.com>
2023-03-01 18:48:26 +0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["upcdn.io", "replicate.delivery", "replicate-delivery.moeyy.xyz", "age-ai.moeyy.cn", "photo-restorer.moeyy.cn", "paint-by-text.moeyy.cn", "scribble-diffusion.moeyy.cn", "user-images.githubusercontent.com"],
+  },
+}
+
+module.exports = nextConfig
