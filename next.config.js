d0e176ee6012a5fe97be7eb308d9664dfd58e767
moeyys <58381898+moeyys@users.noreply.github.com>
2023-03-01 21:08:01 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["upcdn.io", "replicate.delivery", "replicate-delivery.moeyy.xyz", "age-ai.moeyy.cn", "photo-restorer.moeyy.cn", "paint-by-text.moeyy.cn", "scribble-diffusion.moeyy.cn", "user-images.githubusercontent.com", "extrapolate.moeyy.workers.dev"],
+    domains: ["upcdn.io", "replicate.delivery", "replicate-delivery.moeyy.xyz", "age-ai.moeyy.cn", "photo-restorer.moeyy.cn", "paint-by-text.moeyy.cn", "scribble-diffusion.moeyy.cn", "user-images.githubusercontent.com", "ageai-images.moeyy.workers.dev"],
   },
 }
 
